71d5c819d4d765617b4e41074a0f1f518782e771
Boris Gribkov <boris.gribkov@devol.network>
2024-02-23 11:59:17 +0400

    Fix project

---
+++ example/dart_smart_contract_error_generator_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_smart_contract_error_generator/dart_smart_contract_error_generator.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/dart_smart_contract_error_generator_test.dart
@@ -0,0 +1,16 @@
+import 'package:dart_smart_contract_error_generator/dart_smart_contract_error_generator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
